fc3e9a4008dbc01bc27868825fbc84159b28c541
jeremy.jones <jjones@stewartsshops.local>
2023-09-28 16:36:32 -0400

    Split classes into their own files.

--- src/main/kotlin/io/spektacle/springxmldemo/config/KafkaConfig.kt
+++ src/main/kotlin/io/spektacle/springxmldemo/config/KafkaConfig.kt
@@ -1,17 +1,11 @@
 package io.spektacle.springxmldemo.config
 
-import io.spektacle.springxmldemo.serializers.POSJournalSerializer
 import org.apache.kafka.clients.admin.AdminClientConfig
 import org.apache.kafka.clients.admin.NewTopic
-import org.apache.kafka.clients.producer.ProducerConfig
-import org.apache.kafka.common.serialization.StringSerializer
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.kafka.core.DefaultKafkaProducerFactory
 import org.springframework.kafka.core.KafkaAdmin
-import org.springframework.kafka.core.KafkaTemplate
-import org.springframework.kafka.core.ProducerFactory
 
 @Configuration
 class KafkaConfig {
@@ -28,20 +22,4 @@ class KafkaConfig {
 
     @Bean
     fun businessDay(): NewTopic = NewTopic("business-day", 1, 1.toShort())
-}
-
-@Configuration
-class KafkaProducerConfig(
-    @Value(value = "\${spring.kafka.bootstrap-servers}")
-    private val servers: String
-){
-    @Bean
-    fun producerFactory(): ProducerFactory<String, Any> = DefaultKafkaProducerFactory(mutableMapOf<String, Any>(
-        ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to servers,
-        ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java,
-        ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to POSJournalSerializer::class.java
-    ))
-
-    @Bean
-    fun kafkaTemplate() = KafkaTemplate(producerFactory())
 }
---
+++ src/main/kotlin/io/spektacle/springxmldemo/config/KafkaProducerConfig.kt
@@ -0,0 +1,27 @@
+package io.spektacle.springxmldemo.config
+
+import io.spektacle.springxmldemo.serializers.POSJournalSerializer
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.core.ProducerFactory
+
+@Configuration
+class KafkaProducerConfig(
+    @Value(value = "\${spring.kafka.bootstrap-servers}")
+    private val servers: String
+){
+    @Bean
+    fun producerFactory(): ProducerFactory<String, Any> = DefaultKafkaProducerFactory(mutableMapOf<String, Any>(
+        ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to servers,
+        ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java,
+        ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to POSJournalSerializer::class.java
+    ))
+
+    @Bean
+    fun kafkaTemplate() = KafkaTemplate(producerFactory())
+}
